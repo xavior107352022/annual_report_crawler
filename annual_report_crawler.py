996f1cd25622d17b0e97c27dbf16404c42699595
xaviorcathaysite <xavior@cathaysite.com.tw>
2022-10-31 09:14:33 +0800

    Initial commit

---
+++ annual_report_crawler.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import os
+import pandas as pd
+import pyodbc
+import time
+
+save_path = '\\Share_Holder_Ann\\'
+if not os.path.exists(os.getcwd() + save_path):
+    os.makedirs(os.getcwd() + save_path)
+
+server = 'XAVIOR\SQLEXPRESS'
+database = 'cmoney'
+# cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database)
+# cursor = cnxn.cursor()
+#
+# sql_query = "SELECT * FROM firm_info_listed;"
+# df_listed = pd.read_sql_query(sql_query, cnxn)
+# sql_query = "SELECT * FROM firm_info_delisted;"
+# df_delisted = pd.read_sql_query(sql_query, cnxn)
+# df = pd.concat([df_listed, df_delisted])
+
+ChromeDriverManager(path='./chromedriver/').install()
+chrome_options = Options()
+chrome_options.headless = True
+
+year_list = [str(x) for x in range(109, 112)]
+for year in year_list:
+    print('-' * 100)
+    print('-' * 100)
+    print('-' * 100)
+    print('processing : {}'.format(year))
+    for ticker in df['股票代號']:
+        time.sleep(10)
+        try:
+            print('processing : {}'.format(ticker))
+            driver = webdriver.Chrome(ChromeDriverManager(path='./chromedriver/').install(),
+                                      chrome_options=chrome_options)
+            driver.get('https://www.google.com/?hl=zh_tw')
+            url = 'https://doc.twse.com.tw/server-java/t57sb01?id=&key=&step=1&co_id={}&year={}&seamon=&mtype=F&dtype=F04'.format(
+                ticker, year)
+            driver.get(url)
+            print(url)
+            # 點選PDF URL
+            time.sleep(1)
+            driver.find_element_by_tag_name('a').click()
+            handles = driver.window_handles
+            driver.switch_to.window(handles[1])
+            # 抓取pdf資訊
+            time.sleep(1)
+            link = driver.find_elements_by_tag_name('a')[0].get_attribute('href')
+            name = driver.find_elements_by_tag_name('a')[0].text
+            print(link)
+            # 存成PDF
+            response = requests.get(link, allow_redirects=True)
+            file_name = name
+
+            print(file_name + '-' * 10 + 'ok')
+            with open(os.getcwd() + save_path + file_name, 'wb') as f:
+                f.write(response.content)
+                f.close()
+
+        except:
+            print('processing fail: {}'.format(ticker))
